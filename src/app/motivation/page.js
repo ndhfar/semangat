76584f22409c5d555e34eb023c98420a55ab67e8
ndhfar <nadipadhifa5@gmail.com>
2024-06-24 18:41:04 +0700

    comeback link

--- src/app/motivation/page.js
+++ src/app/motivation/page.js
@@ -4,6 +4,7 @@ import Joy from "../../../public/joy.png";
 import Seulgi from "../../../public/seulgi.png";
 import Wendy from "../../../public/wendy.png";
 import Yeri from "../../../public/yeri.png";
+import Link from "next/link";
 
 export default async function Motivation() {
   const res = await fetch("https://v1.appbackend.io/v1/rows/pGNQxiqsPLby", {
@@ -25,12 +26,13 @@ export default async function Motivation() {
         label="Seulgi"
         additional="-right-5 top-0 -rotate-[135deg]"
       />
-      <Redvelvet
-        member={Wendy}
-        label="Wendy"
-        additional="bottom-0 -left-5 rotate-[30deg]"
-      />
-
+      <Link href="/">
+        <Redvelvet
+          member={Wendy}
+          label="Wendy"
+          additional="bottom-0 -left-5 rotate-[30deg]"
+        />
+      </Link>
       <Redvelvet
         member={Yeri}
         label="Yeri"
